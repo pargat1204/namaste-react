2647f174dbf8ef8d6aae0e4c2204ea6983df3658
unknown <pargat161187120@gmail.com>
2024-02-03 01:59:33 +0530

    vol-04

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,22 @@
 import Card from './Card';
+import { useState } from 'react';
 
 const Main = (props) => {
     const { resData, URL } = props;
+    const [listOfRestaurants, setListOfRestaurants] = useState(resData);
     return (
         <div className='main'>
-            <div className='search'>
-                <input ></input>
-                <span>SEARCH</span>
+            <div className='btns'>
+                <button onClick={() => {
+                    setListOfRestaurants(resData);
+                }}>All Restaurants</button>
+                <button onClick={() => {
+                    const filterRes = resData.filter(res => res["info"]["avgRating"] > 4);
+                    setListOfRestaurants(filterRes);
+                }}>Featured Restaurants</button>
             </div>
             <div className='res-container'>
-                {resData.map((res) => {
+                {listOfRestaurants.map((res) => {
                     return (<Card key={res.info.id} resData={res} URL={URL} />)
                 })}
             </div>
